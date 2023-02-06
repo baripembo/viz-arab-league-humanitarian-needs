e1aa5a4bc44f45a8d82839219da59fc17d3bcf51
baripembo <erikawei@gmail.com>
2023-02-06 16:13:59 -0500

    adding new paid contributions layer

--- src/js/panel.js
+++ src/js/panel.js
@@ -12,9 +12,9 @@ function initCountryPanel() {
   var hrpDiv = $('.country-panel .hrp .panel-inner');
   hrpDiv.children().remove();
   createFigure(hrpDiv, {className: 'funding-required', title: 'HRP Requirement', stat: formatValue(data['#value+funding+hrp+required+usd']), indicator: '#value+funding+hrp+required+usd'});
-  //createFigure(hrpDiv, {className: 'funding-covid-allocation', title: 'HRP Funding', stat: formatValue(data['#value+funding+hrp+total+usd']), indicator: '#value+funding+hrp+total+usd'});
   createFigure(hrpDiv, {className: 'funding-cerf-allocation', title: 'CERF Allocation 2023', stat: formatValue(data['#value+cerf+funding+total+usd']), indicator: '#value+cerf+funding+total+usd'});
   createFigure(hrpDiv, {className: 'funding-cbpf-allocation', title: 'CBPF Allocation 2023', stat: formatValue(data['#value+cbpf+funding+total+usd']), indicator: '#value+cbpf+funding+total+usd'});
+  //createFigure(hrpDiv, {className: 'funding-paid-contributions', title: 'Paid Contributions', stat: formatValue(data['#value+cerf+contributions+total+usd']), indicator: '#value+cerf+contributions+total+usd'});
 
   //inform
   var informDiv = $('.country-panel .inform .panel-inner');
